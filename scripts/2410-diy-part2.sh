97e9a755fde0317f7efd9584cf244802d771c9f4
Lyi. <lyimoexiao@outlook.com>
2025-11-19 01:30:47 +0800

    Change login IP from 192.168.6.1 to 10.24.32.1

--- scripts/2410-diy-part2.sh
+++ scripts/2410-diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify login IP
-# sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.6.1/10.24.32.1/g' package/base-files/files/bin/config_generate
 
 # Modify default SSID
 # sed -i "s/ImmortalWrt-2.4G/Breakwa11/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
